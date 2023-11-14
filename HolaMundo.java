56aaaaae2a395b5226eaa80a7c5db8bb4658c8b4
Bruno Delgado Herrero <delgado.hebru24@triana.salesianos.edu>
2023-11-14 14:01:48 +0100

    Translated to ENG

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,9 @@
 public class HolaMundo {
     
     public static void main(String[] args) {
-        System.out.println("*".repeat(20));
-        System.out.println("Hola mundo!!! brrrr");
-        System.out.println("*".repeat(20));
+        System.out.println("*".repeat(21));
+        System.out.println("Hello world!!! brrrr");
+        System.out.println("*".repeat(21));
     }
 
 }
