81502b15de4d0e081f4f83c6424941027f7735a2
Bruno Delgado Herrero <delgado.hebru24@triana.salesianos.edu>
2023-11-14 13:55:06 +0100

    Guauuuu vaya abuso

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,9 @@
 public class HolaMundo {
     
     public static void main(String[] args) {
+        System.out.println("*".repeat(20));
         System.out.println("Hola mundo!!! brrrr");
+        System.out.println("*".repeat(20));
     }
 
 }
