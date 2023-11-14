81de21bcb3ab967fd1a598e590c00562cd578f44
brrrr1 <150147072+brrrr1@users.noreply.github.com>
2023-11-14 14:00:36 +0100

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,9 @@
 public class HolaMundo {
     
     public static void main(String[] args) {
-        System.out.println("*".repeat(20));
-        System.out.println("Hola mundo!!! brrrr");
-        System.out.println("*".repeat(20));
+        System.out.println("*".repeat(24));
+        System.out.println("* Hola mundo!!! brrrr *");
+        System.out.println("*".repeat(24));
     }
 
 }
